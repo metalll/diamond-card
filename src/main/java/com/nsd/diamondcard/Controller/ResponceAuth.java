685fcb4505bc295525a929e7a1ea3f9686498b08
metalll <metall112@bk.ru>
2017-10-08 12:28:27 +0300

    in progress

---
+++ src/main/java/com/nsd/diamondcard/Controller/ResponceAuth.java
@@ -0,0 +1,16 @@
+package com.nsd.diamondcard.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by nsd on 08.10.17.
+ */
+
+@Controller
+@RequestMapping
+public class ResponceAuth {
+
+
+
+}
---
+++ src/main/java/com/nsd/diamondcard/Model/JSONRequest.java
@@ -0,0 +1,11 @@
+package com.nsd.diamondcard.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nsd on 08.10.17.
+ */
+public class JSONRequest {
+    private String status;
+    private ArrayList data;
+}
