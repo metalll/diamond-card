7201ae81d7bd1c2bce4cc768a4b2a11cd6545cc7
NullStackDev <a.petrov@keepsolid.com>
2017-10-11 16:39:43 +0300

    upgrade

--- src/main/java/com/nsd/diamondcard/Controller/ResponceAuth.java
+++ src/main/java/com/nsd/diamondcard/Controller/ResponceAuth.java
@@ -15,10 +15,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -42,7 +39,7 @@ public class ResponceAuth {
     private DBUser userService;
 
 
-    @GetMapping(RESPONCE_AUTH_PATH)
+    @PostMapping(RESPONCE_AUTH_PATH)
     public String responceGet(){
 
         Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
