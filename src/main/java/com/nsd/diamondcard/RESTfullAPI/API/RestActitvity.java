d2664f49183237f9f2252076fadb1f873e25b1f3
NullStackDev <a.petrov@keepsolid.com>
2017-11-01 15:17:54 +0200

    refactoring activity table

--- src/main/java/com/nsd/diamondcard/RESTfullAPI/API/RestActitvity.java
+++ src/main/java/com/nsd/diamondcard/RESTfullAPI/API/RestActitvity.java
@@ -1,9 +1,12 @@
 package com.nsd.diamondcard.RESTfullAPI.API;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequestMapping(value = "/API")
 public class RestActitvity {
+
+
 
 
 
